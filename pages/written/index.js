7fc47dd176c28a35aad74292d2aaddcc644d98c5
evanmcneill2002-star <evanmcneill2002@gmail.com>
2025-08-13 17:23:39 -0400

    Create index.js

---
+++ pages/written/index.js
@@ -0,0 +1,4 @@
+import Layout from "@/components/Layout";
+export default function Written() {
+  return <Layout><h1 className="text-xl font-semibold">Written Practice</h1><p>Select a section and start a test.</p></Layout>;
+}
