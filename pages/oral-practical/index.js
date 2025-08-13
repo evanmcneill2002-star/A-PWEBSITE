c0b8d2bba9fc73ebb7f96e99cc2d3a639bb62a32
evanmcneill2002-star <evanmcneill2002@gmail.com>
2025-08-13 17:24:11 -0400

    Create index.js

---
+++ pages/oral-practical/index.js
@@ -0,0 +1,4 @@
+import Layout from "@/components/Layout";
+export default function OralPractical() {
+  return <Layout><h1 className="text-xl font-semibold">Oral & Practical Prep</h1><p>Use flashcards and task guides here.</p></Layout>;
+}
